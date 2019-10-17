93ff0b5bae1b06013467b62eb664c0bfe0661dcc
Alexander Korotnyan <alexander.korotnyan@gmail.com>
2019-10-17 18:20:34 +0300

    Added CounterScreen.js screen and Go to Counter button.

---
+++ src/screens/CounterScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, Text, StyleSheet, Button} from 'react-native';
+
+const CounterScreen = () => {
+  
+
+  return <View>
+    <Button title='Increase' onPress={() =>{
+      counter++;
+    }} />
+    <Button title='Decrease' onPress={() => {
+      counter--;  
+    }} />
+    <Text>Counter: {counter}</Text>
+  </View>
+};
+
+const styles = StyleSheet.create({});
+
+export default CounterScreen;
