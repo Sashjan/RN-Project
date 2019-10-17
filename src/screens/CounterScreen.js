12fa505a8ceff7b8cc78f261828c023f53e24082
Alexander Korotnyan <alexander.korotnyan@gmail.com>
2019-10-17 20:03:56 +0300

    Wrong approach is commented: DO NOT DO THAT

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
 
 const CounterScreen = () => {
@@ -6,9 +6,15 @@ const CounterScreen = () => {
 
   return <View>
     <Button title='Increase' onPress={() =>{
+        //don't do this counter++, don't change the state
+        // of the variable directly because it is wrong approach 
+        // and will not work!
       counter++;
     }} />
     <Button title='Decrease' onPress={() => {
+        //don't do this counter--, don't change the state
+        // of the variable directly because it is wrong approach 
+        // and will not work!
       counter--;  
     }} />
     <Text>Counter: {counter}</Text>
