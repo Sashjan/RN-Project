336914789225d0f4eedcd74caca3f0ded291b00a
Alexander Korotnyan <alexander.korotnyan@gmail.com>
2019-10-18 19:51:00 +0300

    Added SquareScreen.js screen and Go to Square button

---
+++ src/screens/SquareScreen.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Button} from 'react-native';
+
+const SquareScreen = () =>{
+
+    return <View>
+      <Text>Square Screen</Text>  
+    </View>
+};
+
+const styles = StyleSheet.create({});
+
+export default SquareScreen;
