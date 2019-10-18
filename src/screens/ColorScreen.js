230c01757875f1d48f7591b2902c4509a1bd93d3
Alexander Korotnyan <alexander.korotnyan@gmail.com>
2019-10-18 11:51:51 +0300

    Added ColorScreen.js screen and Go to Color button

---
+++ src/screens/ColorScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button, View, StyleSheet} from 'react-native';
+
+const ColorScreen = () => {
+
+  return <View>
+    <Button title='Add a Color' />  
+  </View>
+};
+
+const styles = StyleSheet.create({});
+
+export default ColorScreen;
--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,21 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
 
 const CounterScreen = () => {
-  
+  const[counter, setCounter]= useState(0);
 
   return <View>
     <Button title='Increase' onPress={() =>{
-        //don't do this counter++, don't change the state
-        // of the variable directly because it is wrong approach 
-        // and will not work!
-      counter++;
+      setCounter(counter + 1);
     }} />
     <Button title='Decrease' onPress={() => {
-        //don't do this counter--, don't change the state
-        // of the variable directly because it is wrong approach 
-        // and will not work!
-      counter--;  
+      setCounter(counter - 1);
     }} />
     <Text>Counter: {counter}</Text>
   </View>
