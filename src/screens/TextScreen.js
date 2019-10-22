578f2acfad39dffed37f5caec554e4d011497cce
Alexander Korotnyan <alexander.korotnyan@gmail.com>
2019-10-22 23:26:23 +0300

    modified to Enter Password from Enter Your Name

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, View, StyleSheet, TextInput} from 'react-native';
 
 const TextScreen = () => {
+  const [password, setPassword] = useState('');
+
   return <View>
+    <Text>Enter Password:</Text>
     <TextInput style={styles.input}
       autoCapitalize="none"
       autoCorrect={false}
+      value={password}
+      onChangeText={(newValue) => setPassword(newValue)}
     />
+    {password.length > 5 ? null : <Text>Password should be grater than 5 character</Text>}
   </View>
 };
 
