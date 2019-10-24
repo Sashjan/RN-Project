d23b1e361260cc66838eada0ef4efc2f3e18a9a7
Alexander Korotnyan <alexander.korotnyan@gmail.com>
2019-10-25 00:25:26 +0300

    added styles and "Child" elements

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,11 +3,23 @@ import {Text, View, StyleSheet} from 'react-native';
 
 const BoxScreen = () => {
 
-    return <View>
-      <Text>Box Screen</Text>  
+    return <View style={styles.viewStyle}>
+      <Text style={styles.textStyle}>Child #1</Text>
+      <Text style={styles.textStyle}>Child #2</Text>
+      <Text style={styles.textStyle}>Child #3</Text>
     </View>
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    viewStyle:{
+      borderWidth: 3,
+      borderColor: 'black',
+      alignItems: 'flex-start'
+    },
+    textStyle:{
+      borderWidth: 3,
+      borderColor: 'red'
+    }
+});
 
 export default BoxScreen;
